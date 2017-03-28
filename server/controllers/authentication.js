3fcbf8b9e293b26968a700ff9e6c872792998349
Aloak <aloak92@gmail.com>
2017-03-28 11:25:27 -0700

    remove module imports for non existant files

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,8 +4,6 @@ const config = require('../config');
 const bcrypt = require('bcrypt-nodejs');
 const Promise = require('bluebird');
 
-db.connect(err => console.log('DB Connected'));
-
 function generateToken(user) {
   const timeStamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timeStamp }, config.secret);
@@ -20,11 +18,11 @@ exports.signup = function (req, res, next) {
   if (!email || !username || !password) {
     return res.status(422).send({ error: 'You must provide email, username and password' });
   } 
-  const usernameQuery = `SELECT * from users WHERE username = "${username}";`
-  const emailQuery = `SELECT * from users WHERE email = "${email}";`
-  const query = Promise.promisify(db.query.bind(db))
+  const usernameQuery = `SELECT * from users WHERE username = "${username}";`;
+  const emailQuery = `SELECT * from users WHERE email = "${email}";`;
+  const query = Promise.promisify(db.query.bind(db));
+
   Promise.all([query(emailQuery), query(usernameQuery)]).then((results) => {
-    console.log(results[0])
     if (results[0].length) {
       return res.status(422).send({ error: 'Email is in use' });
     }
