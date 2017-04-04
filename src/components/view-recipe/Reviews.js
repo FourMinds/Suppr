8cf525371198ede9ebd6d546a31acda23345861e
Bluzkry <zou.alexander@gmail.com>
2017-04-03 20:22:46 -0700

    Make submit button for reviews only function when user chooses a rating
    Even if user writes a review without selecting a rating, the submit button won't function

--- src/components/view-recipe/Reviews.js
+++ src/components/view-recipe/Reviews.js
@@ -6,8 +6,16 @@ import { reduxForm, Field } from 'redux-form';
 const ratingField = rating => (
   <fieldset className="form-group">
     <label className="mr-sm-2">Rating </label><br/>
-    <select {...rating.input} className="rounded-0 custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
-      <option disabled value="Choose...">Choose...</option>
+    {/*parent method validateRating is called onChange; additionally, rating.input.onChange(event) is called to keep Redux form functionality*/}
+    <select
+      {...rating.input}
+      className="rounded-0 custom-select mb-2 mr-sm-2 mb-sm-0"
+      id="inlineFormCustomSelect"
+      onChange={event => {
+        rating.validateRating(event);
+        rating.input.onChange(event);
+      }}>
+      <option select disabled value="Choose...">Choose...</option>
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -25,14 +33,41 @@ const reviewField = review => (
 );
 
 class Reviews extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      disableRating: true
+    };
+
+    this.disableSubmit = this.disableSubmit.bind(this);
+    this.validateRating = this.validateRating.bind(this);
+  }
+
+  disableSubmit() {
+    return this.state.disableRating
+  }
+
   handleFormSubmit(formProps) {
     const { id } = this.props.recipe ? this.props.recipe : '';
     const { username } = this.props;
     this.props.postReview({...formProps, recipeId: id, username});
   }
 
+  // rating enabled if form value isn't Choose
+  validateRating(event) {
+    if (event.target.value !== "Choose...") {
+      this.setState({
+        disableRating: false
+      });
+    }
+  }
+
   render() {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, isRequired, pristine, submitting } = this.props;
+    // when pristine, button is disabled
+    // user can only press submit button when a rating is chosen
+    // this happens through validateRating, which changes the disableRating state; then whether or not the button is disabled depends on disableSubmit
     return (
       <div className="card-block">
         {this.props.reviews
@@ -47,11 +82,15 @@ class Reviews extends Component {
           : null}
         <form
           className="card-block"
-          onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <Field name="rating" component={ratingField}></Field>
+          onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+          validators={{
+            rating: isRequired,
+          }}>
+          <Field name="rating" component={ratingField} validateRating={this.validateRating}></Field>
           <Field name="review" component={reviewField}></Field>
-          <button className="btn btn-primary" disabled={pristine || submitting}>Submit</button>
+          <button className="btn btn-primary" disabled={pristine || this.disableSubmit() || submitting}>Submit</button>
         </form>
+        <p>{ratingField}</p>
       </div>
     )
   }
@@ -65,6 +104,7 @@ function mapStatetoProps(state) {
   }
 }
 
+// without initial values set to 'Choose...', the initial value of the rating is set to '1'
 export default connect(mapStatetoProps, actions)(reduxForm({
   form: 'reviews',
   initialValues: {rating: 'Choose...'}
