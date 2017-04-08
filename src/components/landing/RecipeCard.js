18bc67ef9a046baa24fb38e8aefc637ce688660c
Matt Ridge <mattrid@gmail.com>
2017-04-08 16:42:42 -0700

    filled out basic info for now

--- src/components/landing/RecipeCard.js
+++ src/components/landing/RecipeCard.js
@@ -6,7 +6,7 @@ class RecipeCard extends Component {
   render() {
     const {username, id, name, difficulty, cook_time, prep_time, servings, image, description} = this.props.recipe
     const recipeLink = `/recipe/${id}`
-    
+
     return (
       <div className="card" style={{width: '20em'}}>
         <Link to={recipeLink}><img className="card-img-top" src={image} alt="Card image cap" /></Link>
@@ -15,7 +15,7 @@ class RecipeCard extends Component {
           <p className="card-text">{description}</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item">Basic Info</li>
+          <li className="list-group-item"><div className='recipe-card-stats'><img className="recipe-stats-icon" src="/assets/prep.png"/>Prep: {prep_time}</div>  |  <div className='recipe-card-stats'><img className="recipe-stats-icon" src="/assets/pot.png"/>Cook: {cook_time}</div> </li>
         </ul>
         <div className="card-block">
           <AuthorTile username={username} />
