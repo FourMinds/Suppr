13c4027bc42609fd1ff8b425254740be4c6f3a7e
kurthoefer <kurthoefer@KURTs-MacBook-Pro.local>
2017-04-08 11:55:50 -0700

    fixed conflicts and cleaned up console logs and redundant code

--- src/components/view-recipe/RecipeTile.js
+++ src/components/view-recipe/RecipeTile.js
@@ -51,16 +51,6 @@ class RecipeTile extends Component {
           <AuthorTile username={username} />
           
           <RecipeStats />
-          {/*<div className="recipe-stats-box">
-            <div className="recipe-stats-box-column">
-              <div className="recipe-stats-box-column-row"><img className="recipe-stats-icon" src="/assets/prep.png"/><div>Prep<br />{prepTime} min</div></div>
-              <div className="recipe-stats-box-column-row"><img className="recipe-stats-icon" src="/assets/pot.png"/><div>Cook<br />{cookTime} min</div></div>
-            </div>
-            <div className="recipe-stats-box-column">
-              <div className="recipe-stats-box-column-row"><img className="recipe-stats-icon" src="/assets/tray.png"/><div>Serves<br />{servings}</div></div>
-              <div className="recipe-stats-box-column-row"><img className="recipe-stats-icon" src="/assets/chef.png"/><div>Skill<br />{difficulty}</div></div>
-            </div>
-          </div>*/}
 
         </div>
       </div>
