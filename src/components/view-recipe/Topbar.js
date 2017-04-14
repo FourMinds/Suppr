8ccc6083d92de90210c389cf35296ef2b302e610
kurthoefer <kurthoefer@KURTs-MacBook-Pro.local>
2017-04-14 15:01:44 -0700

    added two new files for rendering rating animation and housing rating and favoriting functionality

---
+++ src/components/view-recipe/Topbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+
+
+
+
+
+
+
+
+
+function mapStateToProps(state) {
+  return {
+    
+  };
+}
+
+export default connect(mapStateToProps, actions)(Topbar);
