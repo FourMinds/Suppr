2a4a48d6f678f919da30ce54cf47c6867a761a1f
Aloak <aloak92@gmail.com>
2017-04-14 20:40:56 -0700

    change profile picture on author tile

--- src/reducers/profile-reducer.js
+++ src/reducers/profile-reducer.js
@@ -1,5 +1,6 @@
 import {
-  GET_PROFILE
+  GET_PROFILE,
+  GET_PROFILE_USER
 } from '../actions/types';
 const initialState = {
   data: {
@@ -13,6 +14,10 @@ export default function(state = initialState, action) {
   switch(action.type) {
     case GET_PROFILE:
       return { ...state, data: action.payload };
+    case GET_PROFILE_USER:
+      const newState = {...state}
+      newState[action.payload.username] = action.payload.data
+      return newState;
     default:
       return state;
   }
