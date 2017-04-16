d7640bc7c939b9800b9504b91d2acfb488f498b8
Aloak <aloak92@gmail.com>
2017-04-15 19:58:54 -0700

    add styles to follow tile component

--- src/components/profile/profile-own/FollowTile.js
+++ src/components/profile/profile-own/FollowTile.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import * as actions from '../../../actions';
 
 class FollowTile extends Component {
+  componentWillMount() {
+    this.props.getUserInfo(this.props.user)
+    this.props.getProfileByUsername(this.props.user);
+  }
 
   renderButtonCaption() {
     const { follows } = this.props.followList;
@@ -12,8 +16,32 @@ class FollowTile extends Component {
     return 'Follow';
   }
 
+  renderInfo() {
+    const { user } = this.props;
+    if(this.props.info[user]) {
+      const {favoritesCount, followersCount, followsCount, recipesCount, sporksCount} = this.props.info[user]
+      return (
+        <div className="author-stats-box">
+          <div className="inner-box-item"><img className="author-stats-icon" src="/assets/salad.png" alt="Recipes" title="recipes"/>{recipesCount}</div>
+          <div className="inner-box-item"><img className="author-stats-icon" src="/assets/spork.png" alt="Sporks" title="sporks"/>{sporksCount}</div>
+          <div className="inner-box-item"><img className="author-stats-icon" src="/assets/follower.png" alt="Followers" title="followers"/>{followersCount}</div>
+          <div className="inner-box-item"><img className="author-stats-icon" src="/assets/favorited.png" alt="Favorited count" title="likes"/>{favoritesCount}</div>
+        </div>
+      )
+    }
+  }
+
   handleFollowButton() {
     this.props.postFollow({ username: this.props.username, followName: this.props.user })
+  }
+
+  getProfilePic() {
+    const { username } = this.props;
+    if(this.props.profile[username] && this.props.profile[username].image) {
+      return this.props.profile[username].image
+    } else {
+      return 'https://secure.gravatar.com/avatar/6e9387de9c9dfa657aa9b518d92e6871?d=https%3A//daks2k3a4ib2z.cloudfront.net/img/profile-user.png'
+    }
   }
 
   render() {
@@ -22,12 +50,13 @@ class FollowTile extends Component {
       <div className="col-md-4">
         <div className="well well-sm">
             <div className="media">
-                <a className="thumbnail pull-left" href="#">
-                  <img className="media-object" src="http://placehold.it/80" alt="placeholder" />
+                <a className="thumbnail pull-left profile-img-container" href="#">
+                  <img className="media-object" src={this.getProfilePic.call(this)} alt="placeholder" />
                 </a>
                 <div className="media-body">
-                  <h4 className="media-heading">{this.props.user}</h4>
-                <p><span className="label label-info">10 photos</span> <span className="label label-primary">89 followers</span></p>
+                  <h4 className="media-heading" style={{marginLeft: '15px'}}>{this.props.user}</h4>
+
+                <p>{this.renderInfo()}</p>
                   <p>
                     <a href={userLink} className="btn btn-xs btn-default">View Profile</a>
                     <a href="#" className="btn btn-xs btn-default" onClick={this.handleFollowButton.bind(this)}>{this.renderButtonCaption()}</a>
@@ -47,7 +76,9 @@ function mapStateToProps(state) {
     viewFollows: state.follows.dataForUser,
     username: state.auth.username,
     followList: state.follows.data,
-    favorites: state.favorites.dataForUser
+    favorites: state.favorites.dataForUser,
+    profile: state.profile,
+    info: state.userInfo
   };
 }
 
