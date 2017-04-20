d811b79ef7a039a34c05cdb66743b493d09c8a39
Bluzkry <zou.alexander@gmail.com>
2017-04-19 18:11:17 -0700

    Add author names to sporks when appearing

--- src/components/view-recipe/SporkCard.js
+++ src/components/view-recipe/SporkCard.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import AuthorTile from './AuthorTile';
 
 class RecipeCard extends Component {
   render() {
-    const {name, image, description} = this.props;
+    const {name, image, description, username} = this.props;
     return (
       <div className="card spork-card" style={{width: '19em', margin: '5px'}}>
   <img className="card-img-top" src={image} alt="Card caption" />
   <div className="card-block">
     <h5 className="card-title">{name}</h5>
     <p className="card-text" style={{wordWrap: 'break-word'}}>{description}</p>
+  </div>
+  <div className="card-block">
+    <AuthorTile username={username} />
   </div>
 </div>
     )
