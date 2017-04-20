119469a3be3aec7f03965d53c1e4d69798d9f7fc
Aloak <aloak92@gmail.com>
2017-04-19 19:40:22 -0700

    fix bug on spork card info tiles

--- src/components/view-recipe/SporkCard.js
+++ src/components/view-recipe/SporkCard.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import AuthorTile from './AuthorTile';
+import * as actions from '../../actions';
+import { connect } from 'react-redux'
 
-class RecipeCard extends Component {
+class SporkCard extends Component {
+  componentWillMount() {
+    this.props.getUserInfo(this.props.username)
+    this.props.getProfileByUsername(this.props.username)
+  }
+
   render() {
     const {name, image, description, username} = this.props;
     return (
       <div className="card spork-card" style={{width: '19em', margin: '5px'}}>
-  <img className="card-img-top" src={image} alt="Card caption" />
-  <div className="card-block">
-    <h5 className="card-title">{name}</h5>
-    <p className="card-text" style={{wordWrap: 'break-word'}}>{description}</p>
-  </div>
-  <div className="card-block">
-    <AuthorTile username={username} />
-  </div>
-</div>
+        <img className="card-img-top" src={image} alt="Card caption" />
+        <div className="card-block">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text card-nowrap">{description}</p>
+        </div>
+        <div className="card-block top-border-box" >
+          <AuthorTile username={username} />
+        </div>
+      </div>
     )
   }
 }
 
-export default RecipeCard;
+export default connect(null, actions)(SporkCard);
